e688a1ba28e91f68566e88f5fe7aec58dc5cf4cd
Rick Fletcher <fletch@pobox.com>
2011-04-06 12:53:49 -0700

    Added viglink setup script

---
+++ bash/categories/viglink.sh
@@ -0,0 +1,16 @@
+##
+# env. settings for viglink machines
+#
+# see $HOSTS_VIGLINK in bashrc.sh
+#
+
+# env. variables -------------------------------------------------------
+
+export VL_WORKSPACE=~/Code/viglink/
+
+# aliases --------------------------------------------------------------
+
+alias cdv="cd $VL_WORKSPACE";
+
+# functions ------------------------------------------------------------
+
