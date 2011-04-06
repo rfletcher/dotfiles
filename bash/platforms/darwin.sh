715440ea0197e0d97ab6592a096095b51f037e98
Rick Fletcher <fletch@pobox.com>
2011-04-06 13:03:06 -0700

    Added aliases from g* to * for certain coreutils

--- bash/platforms/darwin.sh
+++ bash/platforms/darwin.sh
@@ -12,3 +12,9 @@ export DISPLAY=:0.0
 alias ql='qlmanage -p "$@" >& /dev/null';
 alias sort='sort -k 1,1'
 alias top='top -o cpu'
+
+# coreutils (alias <cmd> to coreutils' g<cmd>)
+alias expr='gexpr'
+alias ls='gls --color'
+alias readlink='greadlink'
+alias seq='gseq'
