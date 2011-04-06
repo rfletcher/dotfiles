9b2c824a32c6deec9633a1699c0081261d4db1f4
Rick Fletcher <fletch@pobox.com>
2011-04-06 13:20:05 -0700

    Updated "ex" function

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -190,14 +190,12 @@ function geoip() {
 function ex() {
   if [ -f "$1" ] ; then
     case "$1" in
-      *.tar.bz2)   tar xcjf "$1"   ;;
-      *.tar.gz)    tar zxf  "$1"   ;;
+      *.tar)            tar xvf "$1"  ;;
+      *.tar.bz2|*.tbz2) tar xvjf "$1" ;;
+      *.tar.gz|*.tgz)   tar zvxf "$1" ;;
       *.bz2)       bunzip2 "$1"    ;;
       *.rar)       unrar x "$1"    ;;
       *.gz)        gunzip "$1"     ;;
-      *.tar)       tar xcf "$1"    ;;
-      *.tbz2)      tar xcjf "$1"   ;;
-      *.tgz)       tar xczf "$1"   ;;
       *.zip)       unzip "$1"      ;;
       *.Z)         uncompress "$1" ;;
       *.7z)        7z x "$1"       ;;
