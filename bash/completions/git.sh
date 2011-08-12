a2164421a897ebc8b42451b4d9e221b3e0d76c46
Rick Fletcher <fletch@pobox.com>
2011-08-11 23:44:46 -0700

    Don't show upstream status when it's equal

--- bash/completions/git.sh
+++ bash/completions/git.sh
@@ -195,7 +195,7 @@ __git_ps1_show_upstream ()
 		"") # no upstream
 			p="" ;;
 		"0	0") # equal to upstream
-			p="=" ;;
+			p="" ;;
 		"0	"*) # ahead of upstream
 			p=">" ;;
 		*"	0") # behind upstream
@@ -208,7 +208,7 @@ __git_ps1_show_upstream ()
 		"") # no upstream
 			p="" ;;
 		"0	0") # equal to upstream
-			p=" u=" ;;
+			p="" ;;
 		"0	"*) # ahead of upstream
 			p=" u+${count#0	}" ;;
 		*"	0") # behind upstream
