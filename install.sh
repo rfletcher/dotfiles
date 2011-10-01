c6d0eac4819877a1278b6f929ba07202c26ebb3a
Rick Fletcher <fletch@pobox.com>
2011-10-01 14:28:21 -0700

    install.sh handles files in subdirectories

--- install.sh
+++ install.sh
@@ -8,16 +8,24 @@ INSTALL_SCRIPT=$(basename "$BASH_SOURCE")
 # get the real path to .dotfiles (`readlink -f` doesn't work in BSD)
 DOTFILES_DIR=$(unset CDPATH && cd $(dirname "$BASH_SOURCE") && pwd -P)
 
-for FILE in "$DOTFILES_DIR"/*; do
+function fatal {
+  MSG="$1"
+  echo "$MSG" >&2
+  exit 1
+}
+
+# find files, ignoring hidden files and directories
+find "$DOTFILES_DIR" -type f | sed "s#${DOTFILES_DIR}/##" | grep -vE "(^|/)\." | while read FILE; do
+  DEST=$(dirname "$FILE")
   FILENAME=$(basename "$FILE")
 
-  # if it's not the install script itself...
   if [[ "$FILENAME" != "$INSTALL_SCRIPT" ]]; then
-    # complain about non-symlinks that are in the way
-    if [ -e ~/".$FILENAME" ] && [ ! -L ~/".$FILENAME" ]; then
-      echo "error: $FILENAME is in the way" >&2
-    else
-      rm "$FILENAME" && ln -s "$FILE" ~/".$FILENAME"
+    if [[ "$DEST" != "." ]]; then
+      mkdir -p ~/".${DEST}" || fatal "Couldn't make '$DEST'"
     fi
+
+    # an old symlink in the way?
+    [[ -L ~/".${FILE}" ]] && rm ~/".${FILE}"
+    ln -s "${DOTFILES_DIR}/${FILE}" ~/".${FILE}" || fatal "Couldn't link '$FILE'"
   fi
 done
