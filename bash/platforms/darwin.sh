58a54ff2b726521ccac125e079cdcfc5e48d7ce3
Rick Fletcher <fletch@pobox.com>
2015-11-24 19:28:33 -0500

    Various new variables and aliases

--- bash/platforms/darwin.sh
+++ bash/platforms/darwin.sh
@@ -4,16 +4,18 @@
 
 # env. variables ------------------------------------------------------------
 
-# put /usr/local/{s,}bin in front so Homebrew-installed binaries take precidence
-export PATH="/usr/local/bin:/usr/local/bin:${PATH}"
-export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
-
 # for the occasional X11 app
 export DISPLAY=:0.0
 
-export JAVA_HOME="$(/usr/libexec/java_home 2>/dev/null)"
+export JAVA_HOME="$(/usr/libexec/java_home -v 1.7 2>/dev/null)"
+export NPM_ROOT=/usr/local/share/npm
 export RBENV_ROOT=/usr/local/var/rbenv
-export PATH=".bundle/binstubs:${RBENV_ROOT}/shims:/usr/local/share/npm/bin:$PATH"
+export VAGRANT_DEFAULT_PROVIDER=vmware_fusion
+
+# put /usr/local/{s,}bin in front so Homebrew-installed binaries take precidence
+export PATH="/usr/local/bin:/usr/local/bin:${PATH}"
+# extend the PATH
+export PATH="${RBENV_ROOT}/shims:${NPM_ROOT}/bin:${PATH}"
 
 # aliases -------------------------------------------------------------------
 
@@ -24,7 +26,9 @@ alias mail='open "mailto:?body=$(cat - | stripansi | urlencode)"'
 alias ql='qlmanage -p "$@" >& /dev/null'
 alias sort='sort -k 1,1'
 alias top='top -o cpu'
-alias upgrade='brew update && brew upgrade'
+alias upgrade='brew update && brew upgrade --all'
+# work around a vagrant bug: https://github.com/mitchellh/vagrant/issues/4930
+alias vagrant-destroy-all='vagrant destroy $(vagrant status | grep vmware_fusion | grep -v "not created" | awk "{ print \$1 }")'
 
 # coreutils (alias <cmd> to coreutils' g<cmd>)
 alias expr='gexpr'
