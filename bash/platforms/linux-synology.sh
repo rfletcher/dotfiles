03ae37ef96934ddf046f691fd15bb1744d884744
Rick Fletcher <fletch@pobox.com>
2015-11-24 19:44:04 -0500

    Fix git binary path

--- bash/platforms/linux-synology.sh
+++ bash/platforms/linux-synology.sh
@@ -4,6 +4,7 @@
 
 # env. variables ------------------------------------------------------------
 
+export GIT_SSH="/usr/bin/ssh"
 # put /usr/local/{s,}bin in front so Homebrew-installed binaries take precidence
 export PATH="/opt/sbin:/opt/bin:${PATH}"
 
