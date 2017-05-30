7c71816475ebc3bd9d70a6887aad9a02477f477e
Rick Fletcher <fletch@pobox.com>
2017-05-30 11:03:40 -0400

    Add a notify() function, for macOS notifications

--- bash/platforms/darwin.sh
+++ bash/platforms/darwin.sh
@@ -34,3 +34,39 @@ alias readlink='greadlink'
 alias seq='gseq'
 
 # functions -----------------------------------------------------------------
+
+function notify() {
+  local TN_OPTS=()
+
+  local MESSAGE=""
+  local TITLE=""
+  local SUBTITLE=""
+
+  function _parse_options() {
+    local OPTIND=0
+
+    while getopts ":s:t:" opt; do
+      case $opt in
+        s) SUBTITLE="$OPTARG";;
+        t) TITLE="$OPTARG";;
+      esac
+    done
+
+    shift $((OPTIND - 1))
+
+    MESSAGE="$1"
+  }
+
+  function _set_tn_opt() {
+    TN_OPTS=( "${TN_OPTS[@]}" "-${1}" "${2}" )
+  }
+
+  _parse_options "$@"
+
+  _set_tn_opt message "$MESSAGE"
+
+  [[ "$SUBITTLE" == "" ]] || _set_tn_opt title "$TITLE"
+  [[ "$TITLE" == "" ]]    || _set_tn_opt subtitle "$SUBTITLE"
+
+  terminal-notifier "${TN_OPTS[@]}"
+}
