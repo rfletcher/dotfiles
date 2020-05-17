3b45127ea77c07eebdebfe9235328b92e40aeff2
Rick Fletcher <fletch@pobox.com>
2020-05-17 09:21:55 -0400

    Remove unused `js` alias

--- bash/platforms/linux-ubuntu.sh
+++ bash/platforms/linux-ubuntu.sh
@@ -6,7 +6,6 @@
 
 # aliases -------------------------------------------------------------------
 
-alias js='smjs'
 alias rsync='rsync --skip-compress=avi/mp3/mkv/m4v'
 # fix backspace (found: http://ubuntuforums.org/showthread.php?t=90910)
 alias screen='TERM=screen screen'
