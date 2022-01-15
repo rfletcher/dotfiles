875badfd8cdf54104f41ead76663b8fd7a84a2bc
Rick Fletcher <fletch@pobox.com>
2022-01-15 14:29:30 -0500

    Redo integration config

--- bash/global/integrations.sh
+++ bash/global/integrations.sh
@@ -4,47 +4,44 @@
 # integrate third-party shell extensions
 #
 
-# fzf: https://github.com/junegunn/fzf
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+# homebrew
+if which brew &>/dev/null; then
+  eval "$(brew shellenv 2>/dev/null)"
 
-# aws
-which aws &>/dev/null && complete -C aws_completer aws
+  # export NODE_PATH="$(brew --prefix)/lib/node_modules"
+  # export NPM_ROOT="$NODE_PATH"
+
+  # if [[ -d /opt/homebrew ]]; then         # linux
+  #   eval "$(/opt/homebrew/bin/brew shellenv 2>/dev/null | sed 's|/opt/homebrew|/home/linuxbrew/.linuxbrew|')"
+  # elif [[ -x /usr/local/bin/brew ]]; then # macos
+  #   eval "$(/usr/local/bin/brew shellenv 2>/dev/null)"
+  # fi
+fi
+
+if [ "$PS1" ]; then
+  # aws
+  which aws &>/dev/null && complete -C aws_completer aws
+
+  # fzf: https://github.com/junegunn/fzf
+  [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+fi
 
 # cli-privacy
 if which cli-privacy &>/dev/null; then
   source $(which cli-privacy)
 fi
 
-# hass-cli: https://github.com/home-assistant/home-assistant-cli
-if which hass-cli &>/dev/null; then
-  export HASS_SERVER="(not committed)"
-  export HASS_TOKEN="(not committed)"
-
-  eval "$(hass-cli completion bash)"
+if which kubectl-krew &>/dev/null; then
+  export PATH="${PATH}:${HOME}/.krew/bin"
 fi
 
-# homebrew
-if which brew &>/dev/null; then
-  export NODE_PATH="$(brew --prefix)/lib/node_modules"
-  export NPM_ROOT="$NODE_PATH"
-
-  if [[ -d /opt/homebrew ]]; then         # linux
-    eval "$(/opt/homebrew/bin/brew shellenv 2>/dev/null | sed 's|/opt/homebrew|/home/linuxbrew/.linuxbrew|')"
-  elif [[ -x /usr/local/bin/brew ]]; then # macos
-    eval "$(/usr/local/bin/brew shellenv 2>/dev/null)"
-  fi
-fi
-
-# pipx: https://pypi.org/project/pipx
-if which pipx &>/dev/null; then
-  export PIPX_HOME=/usr/local/var/pipx
-  export PIPX_BIN_DIR=/usr/local/bin
-fi
+# # pipx: https://pypi.org/project/pipx
+# if which pipx &>/dev/null; then
+#   export PIPX_HOME=/usr/local/var/pipx
+#   export PIPX_BIN_DIR=/usr/local/bin
+# fi
 
 # rbenv
 if which rbenv &>/dev/null; then
   eval "$(rbenv init -)"
 fi
-
-# vig
-which vig &>/dev/null && eval "$(vig init 2>&1)"
