cc2b71c315046d71f8857eac0eb9ac7ee0aaf6cb
Rick Fletcher <fletch@pobox.com>
2025-10-06 22:25:17 -0400

    Add install.sh --force

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 # link to .dotfiles/* from ~/
 #
 
+FORCE=
 INSTALL_SCRIPT=$(basename "$BASH_SOURCE")
 # get the real path to .dotfiles (`readlink -f` doesn't work on some systems)
 DOTFILES_DIR=$(unset CDPATH && cd $(dirname "$BASH_SOURCE") && pwd -P)
@@ -12,6 +13,10 @@ function die() {
   echo "$1" >&2;
   exit 1
 }
+
+if [[ "$1" == "--force" ]]; then
+  FORCE=1
+fi
 
 # find files, ignoring hidden files and directories
 cd "${DOTFILES_DIR}/files"
@@ -24,7 +29,11 @@ find . -type f | sed "s|^./||" | while read -r FILE; do
   fi
 
   # an old symlink in the way?
-  [[ -L ~/".${FILE}" ]] && rm ~/".${FILE}"
+  if test -e ~/".${FILE}"; then
+    if [[ "$FORCE" == "1" ]] || [[ -L ~/".${FILE}" ]]; then
+      rm ~/".${FILE}"
+    fi
+  fi
   ln -s "${DOTFILES_DIR}/files/${FILE}" ~/".${FILE}" || die "Couldn't link '$FILE'"
 done
 
